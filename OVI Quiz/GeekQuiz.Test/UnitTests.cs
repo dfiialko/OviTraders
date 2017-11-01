074fd9a2fd4779c84a69621f17a7b0caad2d2581
Unknown <denys.fiialko@gmail.com>
2017-11-01 12:52:51 -0500

    ss

---
+++ OVI Quiz/GeekQuiz.Test/UnitTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using GeekQuiz.Models;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using GeekQuiz.Services;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GeekQuiz.Test
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private TriviaContext db;
+        // Add your 2 test methods here
+        [Test]
+        public void WrongAnswer()
+        {
+            // Manually Added Trivia Answer not to initialize database;
+            TriviaAnswer answer = new TriviaAnswer
+            {
+                UserId = "name",
+                OptionId = 13,
+                QuestionId = 4,
+                Id = 48
+            };
+            TriviaOption a = new TriviaOption();
+            List<TriviaOption> options = new List<TriviaOption>();
+            // Manually Added Trivia Option not to initialize database;
+            a.Title = "What fictional company did Nancy Davolio work for?";
+            a.QuestionId = 4;
+            a.Id = 13;
+            a.IsCorrect = false;
+            options.Add(a);
+            
+            var selectedOption = options.FirstOrDefault(o => o.Id == answer.OptionId
+                && o.QuestionId == answer.QuestionId);
+
+            Assert.IsFalse(selectedOption.IsCorrect);
+        }
+
+        [Test]
+        public void RightAnswer()
+        {
+            // Manually Added Trivia Answer not to initialize database;
+            TriviaAnswer answer = new TriviaAnswer
+            {
+                UserId = "name",
+                OptionId = 48,
+                QuestionId = 12
+            };
+            TriviaOption a = new TriviaOption();
+            List<TriviaOption> options = new List<TriviaOption>();
+            // Manually Added Trivia Option not to initialize database;
+            a.Title = "How many function calls did Windows 1.0 approximately have?";
+            a.QuestionId = 12;
+            a.Id = 48;
+            a.IsCorrect = true;
+            options.Add(a);
+            var selectedOption = options.FirstOrDefault(o => o.Id == answer.OptionId
+                && o.QuestionId == answer.QuestionId);
+
+            Assert.IsTrue(selectedOption.IsCorrect);
+        }
+
+    }
+}
---
+++ OVI Quiz/GeekQuiz/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekQuiz.Controllers;
+using GeekQuiz.Models;
+using GeekQuiz.Services;
+using System.Threading.Tasks;
+namespace GeekQuiz.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var questions = new List<string>();
+            using (var context = new TriviaContext())
+            {
+                foreach (var question in context.TriviaQuestions)
+                {
+                    questions.Add(question.Title);
+                }
+            }
+            //  Pass Data to the View
+            ViewData["active"] = ActiveQuestion();
+            ViewData["questions"] = questions;
+            return View();
+        }
+
+        /// <summary>
+        /// Returns Current active Question from TriviaController
+        /// </summary>
+        /// <returns>TriviaController.nextquestion</returns>
+        public string ActiveQuestion()
+        {
+            TriviaController triviaController = new TriviaController();
+            // Asyncrhonous methos requires time to run
+            Task<TriviaQuestion> task = Task.Run(async () => await triviaController.Get());
+            task.Wait();
+            var activeQuestion = task.Result.Title;
+            return activeQuestion;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
